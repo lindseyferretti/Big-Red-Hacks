704ed6302475b7ab33b75b8fc90a64b001636686
Megan Gryboski <31463070+meg5228@users.noreply.github.com>
2017-09-16 23:48:46 -0400

    Emoji mapping complete

--- main.py
+++ main.py
@@ -34,8 +34,7 @@ def upload_photo():
     # Create a new blob and upload the file's content to Cloud Storage.
     photo = request.files['file']
     blob = bucket.blob(photo.filename)
-    blob.upload_from_string(
-            photo.read(), content_type=photo.content_type)
+    blob.upload_from_string(photo.read(), content_type=photo.content_type)
 
     # Make the blob publicly viewable.
     blob.make_public()
@@ -119,14 +118,76 @@ def num_to_emoji(values):
         eistring = "\U0001F920"
         return eistring
     values.pop()
-    print("VALUES" + str(values[0]) + " " + str(values[1]) + " " + str(values[2]) + " " + str(values[3]))
+
+    for val in values:
+        print(val)
     if (values[0] == values[1] and values[1] == values[2] and values[2] == values[3]):
         eistring = "\U0001F643"
         return eistring
+    #Values for Joy as primary emotion
+    if (values.index(max(values)) == 0 and get_difference(values) == 1):
+        eistring = "\U0001F60A"
+        return eistring
+    if (values.index(max(values)) == 0 and get_difference(values) == 2):
+        eistring = "\U0001F603"
+        return eistring
+    if (values.index(max(values)) == 0 and get_difference(values) == 3):
+        eistring = "\U0001F604"
+        return eistring
+    if (values.index(max(values)) == 0 and get_difference(values) == 4):
+        eistring = "\U0001F601"
+        return eistring
+    #Values for Sorrow as primary emtion
+    if (values.index(max(values)) == 1 and get_difference(values) == 1):
+        eistring = "\U00002639"
+        return eistring
+    if (values.index(max(values)) == 1 and get_difference(values) == 2):
+        eistring = "\U0001F61E"
+        return eistring
+    if (values.index(max(values)) == 1 and get_difference(values) == 3):
+        eistring = "\U0001F622"
+        return eistring
+    if (values.index(max(values)) == 1 and get_difference(values) == 4):
+        eistring = "\U0001F62D"
+        return eistring
+    #Values for Anger as primary emotion
+    if (values.index(max(values)) == 2 and get_difference(values) == 1):
+        eistring = "\U0001F624"
+        return eistring
+    if (values.index(max(values)) == 2 and get_difference(values) == 2):
+        eistring = "\U0001F620"
+        return eistring
+    if (values.index(max(values)) == 2 and get_difference(values) == 3):
+        eistring = "\U0001F621"
+        return eistring
+    if (values.index(max(values)) == 2 and get_difference(values) == 4):
+        eistring = "\U0001F47F"
+        return eistring
+    #Values for Suprise as primary emotion
+    if (values.index(max(values)) == 3 and get_difference(values) == 1):
+        eistring = "\U0001F626"
+        return eistring
+    if (values.index(max(values)) == 3 and get_difference(values) == 2):
+        eistring = "\U0001F627"
+        return eistring
+    if (values.index(max(values)) == 3 and get_difference(values) == 3):
+        eistring = "\U0001F633"
+        return eistring
+    if (values.index(max(values)) == 3 and get_difference(values) == 4):
+        eistring = "\U0001F628"
+        return eistring
+
     eistring = emojicodes[values.index(max(values))]
     return eistring
 
-
+def get_difference(values):
+    index = values.index(max(values))
+    max1 = values[index]
+    values[index]=0
+    max2 = max(values)
+    values[index]=max1
+    dif = max1 - max2
+    return dif
 
 if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
